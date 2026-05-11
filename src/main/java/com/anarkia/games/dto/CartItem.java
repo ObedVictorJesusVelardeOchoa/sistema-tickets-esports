fd003c53bf16e9605042a784dd043a88608e3753
Obed Velarde <U23225009@utp.edu.pe>
2026-05-10 20:32:55 -0500

    CartItem.java

---
+++ src/main/java/com/anarkia/games/dto/CartItem.java
@@ -0,0 +1,12 @@
+package com.anarkia.games.dto;
+
+public class CartItem {
+    private String tipo;
+    private int cantidad;
+
+    // Getters y Setters
+    public String getTipo() { return tipo; }
+    public void setTipo(String tipo) { this.tipo = tipo; }
+    public int getCantidad() { return cantidad; }
+    public void setCantidad(int cantidad) { this.cantidad = cantidad; }
+}
